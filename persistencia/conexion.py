b1c26f55a588642cbc6b1a9abd3ec8031ca2d3c0
mcherrera <mcherrera911@gmail.com>
2025-11-22 21:44:30 -0300

    feat: create class to conexion with pysqm driver

---
+++ persistencia/conexion.py
@@ -0,0 +1,29 @@
+import pymysql
+
+class Conexion:
+
+    def __init__(self):
+        self.host = 'localhost'
+        self.user = 'administrador'
+        self.password = 'admin123'
+        self.database = 'ecotech_solutions_company'
+
+    def conectar(self):
+        # Aquí iría la lógica para conectar a la base de datos
+        try:
+            conexion = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database,
+                auth_plugin_map={'caching_sha2_password': pymysql.cursors.DictCursor}
+            )
+            print("Conexión exitosa a la base de datos")
+            return conexion
+        except pymysql.MySQLError as e:
+            print(f"Error al conectar a la base de datos: {e}")
+            return None
+
+if __name__ == '__main__':
+    conexion = Conexion()
+    conexion.conectar()
