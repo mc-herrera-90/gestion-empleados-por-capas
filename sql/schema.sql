1c1ebac345ad4c88d60525ef92842c99d1cf09ff
mcherrera <mcherrera911@gmail.com>
2025-11-22 21:40:48 -0300

    chore(sql): sql DDL init

---
+++ sql/schema.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ecotech_solutions_company;
+USE ecotech_solutions_company;
+
+-- CREATE USER 'administrador'@'localhost' IDENTIFIED BY 'admin123';
+-- GRANT ALL PRIVILEGES ON ecotech_solutions_company.* TO 'administrador'@`localhost`;
