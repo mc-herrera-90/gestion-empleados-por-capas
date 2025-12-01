ecb8ea4a3c90317ce649350d301f1d1b92f61822
mcherrera <mcherrera911@gmail.com>
2025-11-30 23:13:59 -0300

    chore(main): add create schema and user when execute first

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from presentacion.menu_departamento import MenuDepartamento
 from presentacion.menu_principal import MenuPrincipal
+from persistencia.gestor_db import GestorBD
 
 def main():
+    gdb = GestorBD('schema.sql')
+    gdb.crear_esquema()
     menu_principal = MenuPrincipal()
 
 if __name__ == "__main__":
