4cbb841fdfa93523d9de2619ae11085f05134945
mcherrera <mcherrera911@gmail.com>
2025-11-30 22:50:52 -0300

    chore(config): update variables

--- src/config/variables.py
+++ src/config/variables.py
@@ -1,3 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_ROOT = os.getenv('DB_ROOT', 'root')
+DB_ROOT_PASSWORD = os.getenv('DB_ROOT_PASSWORD', 'admin123')
+DB_NAME = os.getenv('DB_NAME', 'mysql')
