a3b054e966542997d7d6575752ddf977c2449452
mcherrera <mcherrera911@gmail.com>
2025-11-30 23:39:11 -0300

    chore(menu): update abstract methods

--- src/presentacion/menu.py
+++ src/presentacion/menu.py
@@ -4,3 +4,22 @@ class Menu(ABC):
     @abstractmethod
     def mostrar_opciones(self):
         pass
+
+    @abstractmethod
+    def mostrarTodos(self):
+        pass
+    @abstractmethod
+    def buscarPorCodigo(self):
+        pass
+    @abstractmethod
+    def buscarPorNombre(self):
+        pass
+    @abstractmethod
+    def modificar(self):
+        pass
+    @abstractmethod
+    def eliminar(self):
+        pass
+    @abstractmethod
+    def volverMenuPrincipal(self):
+        pass
