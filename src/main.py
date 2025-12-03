f885b82cfa5bb918ee2310bb149e94c4981d3e18
marco <mcherrera>
2025-12-03 20:09:51 -0300

    chore(main): update

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from presentacion.menu_departamento import MenuDepartamento
 from presentacion.menu_principal import MenuPrincipal
 from persistencia.gestor_db import GestorBD
 
 def main():
     gdb = GestorBD('schema.sql')
     gdb.crear_esquema()
-    menu_principal = MenuPrincipal()
+    MenuPrincipal()
 
 if __name__ == "__main__":
     main()
