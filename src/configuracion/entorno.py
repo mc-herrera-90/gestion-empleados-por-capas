9f31f51190c70780d800430d58c1537eb34322ee
mcherrera <mcherrera911@gmail.com>
2025-12-07 23:13:08 -0300

    chore(update): remove DB_ROOT, DB_ROOT_PASSWORD, DB_ROOT_NAME

--- src/configuracion/entorno.py
+++ src/configuracion/entorno.py
@@ -18,17 +18,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-ROOT_PATH = Path(__file__).resolve().parent.parent
+ROOT_PATH = Path(__file__).resolve().parents[2]
 
 # Cargar .env
 load_dotenv(dotenv_path=ROOT_PATH / ".env")
 
 # Base de datos
 DB_HOST: str = os.getenv("DB_HOST", "localhost")
-DB_ROOT: str = os.getenv("DB_ROOT", "root")
-DB_ROOT_PASSWORD: str = os.getenv("DB_ROOT_PASSWORD", "password")
-DB_ROOT_NAME: str = os.getenv("DB_ROOT_NAME", "mysql")
-DB_USER: str = os.getenv("DB_USER", "administrador")
+DB_USER: str = os.getenv("DB_USER", "ecotech_admin")
 DB_USER_PASSWORD: str = os.getenv("DB_USER_PASSWORD", "Admin123!")
 DB_NAME: str = os.getenv("DB_NAME", "ecotech_solutions_company")
 
