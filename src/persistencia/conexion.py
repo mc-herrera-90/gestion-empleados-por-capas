5b14f0208b7d89903d710bbbcb54f031f562e30e
mcherrera <mcherrera911@gmail.com>
2025-12-08 01:48:05 -0300

    feat(conexion): add public method to open conexion

--- src/persistencia/conexion.py
+++ src/persistencia/conexion.py
@@ -1,27 +1,39 @@
 import pymysql
 from pymysql.connections import Connection
-from typing import Optional, Iterable, Any
-from configuracion.entorno import DB_HOST, DB_NAME
+from configuracion.entorno import DB_HOST, DB_NAME, DB_USER, DB_USER_PASSWORD
 from configuracion.auditoria import logger_bd
+from persistencia.tipos import QueryResult, SQLParams
 
 
 class Conexion:
 
-    def __init__(self, host: str = DB_HOST, database: str = DB_NAME) -> None:
+    def __init__(
+        self,
+        host: str = DB_HOST,
+        database: str = DB_NAME,
+        user: str = DB_USER,
+        password: str = DB_USER_PASSWORD,
+    ) -> None:
         self._host = host
         self._database = database
+        self._user = user
+        self._password = password
         self._conn: Optional[Connection] = None
 
-    def _conectar(self, user: str, password: str) -> Optional[Connection]:
-        """
-        Crea UNA conexión y la guarda en self._conn
-        """
+    def _conectar(self) -> Optional[Connection]:
+
+        if self._conn and getattr(self._conn, "open", False):
+            return self._conn
+
         try:
             self._conn = pymysql.connect(
-                host=self._host, user=user, password=password, database=self._database
+                host=self._host,
+                user=self._user,
+                password=self._password,
+                database=self._database,
             )
             logger_bd.info(
-                f"🔌 Conexión a MySQL exitosa: {user}@{self._host} -> {self._database}"
+                f"🔌 Conexión a 🐬 MySQL exitosa: {self._user}@{self._host} -> {self._database}"
             )
             return self._conn
 
@@ -31,9 +43,6 @@ class Conexion:
             return None
 
     def _desconectar(self) -> bool:
-        """
-        Cierra la conexión guardada en self._conn
-        """
         if self._conn:
             try:
                 self._conn.close()
@@ -45,16 +54,20 @@ class Conexion:
                 return False
         return False
 
-    def _ejecutar_query(self, query: str, params: Optional[Iterable[Any]] = None):
-        """
-        Ejecuta SQL usando la conexión interna self._conn
-        """
+    def ejecutar_query(
+        self, query: str, params: SQLParams = None
+    ) -> QueryResult | int | None:
         if not self._conn:
+            logger_bd.error(
+                "❌ No se pudo establecer conexión con la BD antes de ejecutar query."
+            )
             raise RuntimeError(
                 "No hay conexión activa. Debes llamar _conectar() primero."
             )
 
         try:
+            logger_bd.debug(f"[Conexion] Ejecutando query: {query} | params: {params}")
+
             with self._conn.cursor() as cur:
                 cur.execute(query, params)
                 self._conn.commit()
@@ -67,3 +80,7 @@ class Conexion:
         except pymysql.MySQLError as e:
             logger_bd.error(f"[Conexion] Error en ejecutar_query: {e}")
             return None
+
+    def abrir(self) -> bool:
+        """Abre la conexión si no está abierta. Devuelve True si se conecta correctamente."""
+        return self._conectar() is not None
